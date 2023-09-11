3685866bb1d84d33aff7bb9f287762893e42ebc8
Neha Singh <nsingh1992@gmail.com>
2023-09-11 20:49:33 +1000

    Remove router for deploying

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import Alert from './Components/Alert';
-import About from './Components/About';
-import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
+// import About from './Components/About';
+// import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
 
 function App() {
   const[mode,setMode] = useState('light');
@@ -39,16 +39,18 @@ function App() {
   }
   return (
     <>
-<Router>
+{/* <Router> */}
 <Navbar title='TextUtils'  aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}></Navbar>
 <Alert alert={alert}></Alert>
-<div className="container my-3">
-  <Routes>
-    <Route exact path='/about' element={<About/>} />
-    <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to be analyzed" mode={mode}/>} />
-  </Routes>
-</div>
-</Router>
+ <div className="container my-3">
+  {/* <Routes> */}
+    {/* <Route exact path='/about' element={<About/>} /> */}
+    {/* <Route exact path="/" element={ */}
+    <TextForm showAlert={showAlert} heading="Enter the text to be analyzed" mode={mode}/>
+    {/* } /> */}
+  {/* </Routes> */}
+ </div> 
+{/* </Router> */}
     </>  
   );
 }
